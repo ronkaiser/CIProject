c286992b3ee145df587fd5122584e921e6c98013
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-11-14 20:24:17 +0200

    edit jenkinsfile

--- whats_going_on.py
+++ whats_going_on.py
@@ -10,7 +10,9 @@ disk = shutil.disk_usage("/")
 path = os.getcwd()
 
 
-system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release, "Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": today, "FreeDisk": disk.free, "Path": path}
+system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release, 
+"Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": today, 
+"FreeDisk": disk.free, "Path": path}
 
 
 json_object = json.dumps(system_info, indent = 4)
