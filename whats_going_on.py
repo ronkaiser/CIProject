852f694ec990db8bb9e641736ea34829a5cefafd
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-11-14 18:07:00 +0200

    python modified
    Signed-off-by: ronkaiser <70015659+ronkaiser@users.noreply.github.com>

--- whats_going_on.py
+++ whats_going_on.py
@@ -3,9 +3,9 @@ from datetime import date
 import json
 
 my_system = platform.uname()
-today = date.today()
+today = str(date.today())
 
-system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release, "Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": datetime.today}
+system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release, "Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": today}
 
 
 json_object = json.dumps(system_info, indent = 4)
