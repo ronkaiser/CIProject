028da750f959d9cf8bf5e234c864247526c01055
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-11-14 19:02:04 +0200

    edit jenkinsfile

--- whats_going_on.py
+++ whats_going_on.py
@@ -1,11 +1,13 @@
 import platform
 from datetime import date
+import os
 import json
 
 my_system = platform.uname()
 today = str(date.today())
+path = os.getcwd()
 
-system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release, "Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": today}
+system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release, "Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": today, "Path": path}
 
 
 json_object = json.dumps(system_info, indent = 4)
