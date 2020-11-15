8c440fcbf658087250cb8a28c6db720942097319
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-11-15 12:15:24 +0200

    edit python file

--- whats_going_on.py
+++ whats_going_on.py
@@ -8,15 +8,17 @@ my_system = platform.uname()
 today = str(date.today())
 tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
 disk = shutil.disk_usage("/")
+key = 'BUILD_NUMBER'
+build = os.getenv(key)
 path = os.getcwd()
 
 
-system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release, 
-"Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": today, 
-"FreeMem": free_m, "FreeDisk": disk.free, "Path": path}
+system_info = {"system": my_system.system, "nodeName": my_system.node, "Release": my_system.release,
+"Version": my_system.version, "Machine": my_system.machine, "Processor": my_system.processor, "Date": today,
+"FreeMem": free_m, "FreeDisk": disk.free, "Build": build, "Path": path}
 
 
 json_object = json.dumps(system_info, indent = 4)
 
 
-print(json_object) 
+print(json_object)
